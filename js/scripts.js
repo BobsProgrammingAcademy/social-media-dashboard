3029464d784fad6f30f714e8d06e1b4b27039c76
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-29 20:19:54 +0100

    Updated scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
