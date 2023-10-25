58ed5d85bb73ba8051389dc5b10897edcb4c69b1
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-10-26 01:03:35 +0200

    Updated JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // SIDEBAR TOGGLE
 
-var sidebarOpen = false;
-var sidebar = document.getElementById('sidebar');
+let sidebarOpen = false;
+const sidebar = document.getElementById('sidebar');
 
 function openSidebar() {
   if (!sidebarOpen) {
